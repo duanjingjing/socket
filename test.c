8f28687191ff84bd398eb6f9550c1cd7d9193a8b
duanjingjing <410001089@qq.com>
2014-06-20 19:31:14 -0700

    canjian

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: canjian
+	> Mail:410001089@qq.com 
+	> Created Time: Fri 20 Jun 2014 07:30:19 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(){
+	printf("hello world");
+	return 0;
+}
